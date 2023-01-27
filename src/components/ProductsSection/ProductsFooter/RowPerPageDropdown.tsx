6ed1c35d1ca696d3737dc4449dda132b0589847e
sunmin <polarlsm1231@gmail.com>
2023-01-27 22:16:31 +0900

    fix: 페이지당 행 개수 엔터키 입력시 바로 반영 안되는 오류

--- src/components/ProductsSection/ProductsFooter/RowPerPageDropdown.tsx
+++ src/components/ProductsSection/ProductsFooter/RowPerPageDropdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { KeyboardEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import { addRowCount } from '../../../redux/pageInfo';
 import { rowPerPage } from '../../../util/constantData';
@@ -11,12 +11,18 @@ export default function RowPerPageDropdown() {
     rowPerPage,
     'rowPerPage'
   );
+  const keyDownHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+    moveSelectItemHandler(e);
+    if (e.key === 'Enter') {
+      dispatch(addRowCount(getCurrentRowPerPage()));
+    }
+  };
   const submitRowPerPage = (index: number) => {
     clickItemHandler(index);
     dispatch(addRowCount(getCurrentRowPerPage()));
   };
   return (
-    <div className="row_per_page_dropdown_container" role="button" tabIndex={0} onKeyDown={moveSelectItemHandler}>
+    <div className="row_per_page_dropdown_container" role="button" tabIndex={0} onKeyDown={keyDownHandler}>
       <div className="row_per_page_select_box">
         <div className="selected">페이지당 행: {getCurrentRowPerPage()}</div>
         <button type="button" className="drop_down_button" onClick={toggleDropDown}>
