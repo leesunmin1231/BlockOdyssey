2c2cdc63a9b0876de4d39dfa6a1054d7647d523a
sunmin <polarlsm1231@gmail.com>
2023-01-28 14:06:15 +0900

    refactor: 페이지 네비게이터 컴포넌트 구조 변환

--- src/util/getPagesNumber.ts
+++
@@ -1,4 +0,0 @@
-export const getPageNumbers = (totalProducts: number, rowPerPage: number) => {
-  const totalPages = Math.ceil(totalProducts / rowPerPage);
-  return Array.from(Array(totalPages).keys());
-};
---
+++ src/util/getPaginationButtons.ts
@@ -0,0 +1,30 @@
+const getRangeList = (start: number, end: number) =>
+  Array.from(Array(end - start + 1).keys()).map((x) => String(x + start));
+
+const getFrontButtons = (currentPage: number) => {
+  if (currentPage - 1 < 4) {
+    return ['1', '2', '3', '4', '5'];
+  }
+  return ['1', '...'];
+};
+
+const getEndButtons = (currentPage: number, totalPages: number) => {
+  if (totalPages - currentPage >= 4) {
+    return ['...', `${totalPages}`];
+  }
+  return getRangeList(totalPages - 4, totalPages);
+};
+
+const getMiddleButtons = (currentPage: number, totalPages: number) => {
+  if (totalPages - currentPage >= 4 && currentPage - 1 >= 4) {
+    return [`${currentPage - 1}`, `${currentPage}`, `${currentPage + 1}`];
+  }
+  return [];
+};
+
+export const getPaginationButtons = (totalPages: number, currentPage: number) => {
+  const frontButtons = getFrontButtons(currentPage);
+  const middleButtons = getMiddleButtons(currentPage, totalPages);
+  const endButtons = getEndButtons(currentPage, totalPages);
+  return [...frontButtons, ...middleButtons, ...endButtons];
+};
