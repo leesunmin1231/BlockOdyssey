c036b7dc352dd75a5da91ebc4e9e286567dd628f
sunmin <polarlsm1231@gmail.com>
2023-01-28 15:01:01 +0900

    fix: 페이지당 행 변경시 페이지 1로 초기화

--- src/components/ProductsSection/ProductsFooter/RowPerPageDropdown.tsx
+++ src/components/ProductsSection/ProductsFooter/RowPerPageDropdown.tsx
@@ -1,6 +1,6 @@
 import React, { KeyboardEvent } from 'react';
 import { useDispatch } from 'react-redux';
-import { addRowCount } from '../../../redux/pageInfo';
+import { addRowCount, addStartPage } from '../../../redux/pageInfo';
 import { rowPerPage } from '../../../util/constantData';
 import { useDropdown } from '../../../hooks/useDropdown';
 import { getCurrentRowPerPage } from '../../../util/getSessionStorage';
@@ -11,15 +11,20 @@ export default function RowPerPageDropdown() {
     rowPerPage,
     'rowPerPage'
   );
+  const submitRowPerPageHanlder = () => {
+    sessionStorage.setItem('page', '1');
+    dispatch(addStartPage(1));
+    dispatch(addRowCount(getCurrentRowPerPage()));
+  };
   const keyDownHandler = (e: KeyboardEvent<HTMLDivElement>) => {
     moveSelectItemHandler(e);
     if (e.key === 'Enter') {
-      dispatch(addRowCount(getCurrentRowPerPage()));
+      submitRowPerPageHanlder();
     }
   };
-  const submitRowPerPage = (index: number) => {
+  const clickRowPerPageHandler = (index: number) => {
     clickItemHandler(index);
-    dispatch(addRowCount(getCurrentRowPerPage()));
+    submitRowPerPageHanlder();
   };
   return (
     <div className="row_per_page_dropdown_container" role="button" tabIndex={0} onKeyDown={keyDownHandler}>
@@ -34,7 +39,7 @@ export default function RowPerPageDropdown() {
           <div className="drop_down_box">
             {rowPerPage.map((rows, index) => (
               <li key={rows} className={rows === getCurrentRowPerPage() ? 'select' : 'non_select'}>
-                <button type="button" onClick={() => submitRowPerPage(index)}>
+                <button type="button" onClick={() => clickRowPerPageHandler(index)}>
                   {rows}
                 </button>
               </li>
